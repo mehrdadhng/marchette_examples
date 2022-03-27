af6bd00239d4234cfe371c367aec62d10ae977e0
mehrdadhng <mehrdadhooshanginaghani@gmail.com>
2022-03-27 12:39:29 +0430

    add install shell

---
+++ ch02/Network Traffic Intensities/install-dependencies.sh
@@ -0,0 +1,8 @@
+mkdir ~/temp
+cd ~/temp
+git clone https://github.com/mininet/mininet.git
+cd mininet
+PYTHON=python3 util/install.sh -a
+sudo pip3 install numpy
+sudo pip3 install matplotlib
+sudo pip3 install sklearn
